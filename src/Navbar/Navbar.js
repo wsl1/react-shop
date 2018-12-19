9469c2286190d6b399398985df37878587e89cd2
Wojciech Słomczyński <w.sl@o2.pl>
2018-12-19 18:22:11 +0100

    initial commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Navbar.scss';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
