7d8b45a1cc89961b5950ea66b93c18e42d066b98
Wojciech Słomczyński <w.sl@o2.pl>
2019-02-04 21:14:44 +0100

    added file for scss variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './variables';
 import './index.css';
 import App from './App';
 import 'normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
---
+++ src/variables.js
@@ -0,0 +1,5 @@
+export default {
+  buttonColor: '#ff5a00',
+  buttonActiveColor: '#ff9c66',
+  mainThemeColor: '#405e8e',
+};
